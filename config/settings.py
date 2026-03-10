65107ac808807b2403e853bdfe4f0145a72e1b6c
iamprecieee <emmypresh777@gmail.com>
2026-03-10 21:44:35 +0100

    feat: add config for postgres db

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,7 @@ setup_logging(log_level=LOGGING_LEVEL, log_file=LOG_FILE)
 # Force Django to use our logging configuration
 LOGGING_CONFIG = None
 
+import dj_database_url
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -107,10 +108,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=env.str("DJANGO_DATABASE_URL", default="sqlite:///db.sqlite3"),
+        conn_max_age=env.int("DATABASE_CONN_MAX_AGE", default=600),
+        conn_health_checks=True,
+    )
 }
 
 
